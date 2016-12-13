e8a21097d4c65ff716041c9ec7492787efce3edf
ChandanaRao13 <chandana.rao13@gmail.com>
2016-12-13 15:25:58 -0800

    Updating Sensor info

--- SensorDataGenerationEvent.sql
+++ SensorDataGenerationEvent.sql
@@ -1,12 +1,6 @@
-SET GLOBAL event_scheduler = ON;
-
-#
-#	An Event to create new sensor data records for all sensors in interval of 15 seconds
-#
-
 DELIMITER |
 
-CREATE EVENT `infraSense-dev`.sensorData
+CREATE EVENT `InfraSense`.sensorData
 ON SCHEDULE EVERY 15 SECOND
 STARTS CURRENT_TIMESTAMP
 ON COMPLETION PRESERVE
@@ -14,21 +8,21 @@ DO
 BEGIN
 DECLARE n INT DEFAULT 0;
 DECLARE i INT DEFAULT 0;
-SELECT COUNT(*) FROM `infraSense-dev`.sensor WHERE Status='running' INTO n;
+SELECT COUNT(*) FROM `InfraSense`.sensor WHERE Status='running' INTO n;
 SET i=0;
 WHILE i<n DO
 	#loop to insert
-	INSERT INTO `infraSense-dev`.sensordata (SensorId, Data, Timestamp)
+	INSERT INTO `InfraSense`.sensordata (SensorId, Data, Timestamp)
 	VALUES(
     (Select s.Sensorid 
-	from `infraSense-dev`.sensor s 
+	from `InfraSense`.sensor s 
 	where s.id = i + 1), 
     (SELECT 
     CASE 
     WHEN u.SensorType = 'Temperature Sensor' THEN ROUND(RAND()*(80-32)+32,2)
-    WHEN u.SensorType = 'Pressure Sensor' THEN ROUND(RAND()*(32-0)+0,2)
-    WHEN u.SensorType = 'Oxygen Sensor' THEN ROUND(RAND()*(32-0)+0,2)
-    WHEN u.SensorType = 'Salinity Sensor' THEN ROUND(RAND()*(32-0)+0,2)
+    WHEN u.SensorType = 'Pressure Sensor' THEN ROUND(RAND()*(850-650)+650,2)
+    WHEN u.SensorType = 'Wind Sensor' THEN ROUND(RAND()*(20-5)+5,2)
+    WHEN u.SensorType = 'Humidity Sensor' THEN ROUND(RAND()*(50-40)+40,2)
     END
     FROM `infraSense-dev`.sensor u
     WHERE u.id = i+1),
